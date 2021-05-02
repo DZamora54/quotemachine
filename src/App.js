b3d7af1b7f7f2d550040dd082c9b2dcd6c3e28f1
DZamora54 <dantezamora54@gmail.com>
2021-05-02 10:17:19 -0400

    updated quote api details

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './Normalize.css'
 import './App.css';
 
 // Link to QuoteGarden
-const randomQuote = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+const randomQuote = "https://quote-garden.herokuapp.com/api/v3/quotes/random";
 
 // Declaring variable to store quote text and quote author
 let myQuote = {
@@ -22,10 +22,10 @@ const getQuote = () => {
       return response.json();
     })
     .then((data) => {
-      // console.log(data.quote.quoteAuthor);
-      // console.log(data.quote.quoteText);
-      myQuote.quoteAuthor = data.quote.quoteAuthor;
-      myQuote.quoteText = data.quote.quoteText;
+      // console.log(data.data[0]["quoteAuthor"]);
+      // console.log(data.data[0]["quoteText"]);
+      myQuote.quoteAuthor = data.data[0]["quoteAuthor"];
+      myQuote.quoteText = data.data[0]["quoteText"];
     });
 };
 // End gets quote from QuoteGarden and stores returned values in myQuote
@@ -54,9 +54,9 @@ class QuoteMachine extends React.Component {
         return response.json();
       })
       .then((data) => {
-        this.setState({ quoteAuthor: data.quote.quoteAuthor });
-        this.setState({ quoteText: data.quote.quoteText });
-        this.setState({twitterUrl: data.quote.quoteText+"-"+data.quote.quoteAuthor})
+        this.setState({ quoteAuthor: data.data[0]["quoteAuthor"] });
+        this.setState({ quoteText: data.data[0]["quoteText"] });
+        this.setState({twitterUrl: data.data[0]["quoteText"]+"-"+data.data[0]["quoteAuthor"]})
       });
   }
 
